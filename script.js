ed172fd700437f73f1106c1797d19e730ff38162
Ron Buttermore <ron.buttermore@gmail.com>
2023-05-17 16:36:28 -0600

    added color coding to hour blocks and event listener for save button

--- script.js
+++ script.js
@@ -1,24 +1,100 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function main() {
+  //set header to current day using dayjs
   $("#currentDay").text(dayjs().format("MMM D, YYYY"));
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+  var currentHour = dayjs().format("H");
+  var nineam = $("#hour-9");
+  var tenam = $("#hour-10");
+  var elevenam = $("#hour-11");
+  var twelvepm = $("#hour-12");
+  var onepm = $("#hour-13");
+  var twopm = $("#hour-14");
+  var threepm = $("#hour-15");
+  var fourpm = $("#hour-16");
+  var fivepm = $("#hour-17");
+
+  //conditional formatting to set colors of input to past/present/future classes
+  if (currentHour == 9){
+    nineam.addClass('present');
+  } else if (currentHour > 9) {
+    nineam.addClass('past');
+  } else {
+    nineam.addClass('future');
+  }
+
+  if (currentHour == 10){
+    tenam.addClass('present');
+  } else if (currentHour > 10) {
+    tenam.addClass('past');
+  } else {
+    tenam.addClass('future');
+  }
+
+  if (currentHour == 11){
+    elevenam.addClass('present');
+  } else if (currentHour > 11) {
+    elevenam.addClass('past');
+  } else {
+    elevenam.addClass('future');
+  }
+
+  if (currentHour == 12){
+    twelvepm.addClass('present');
+  } else if (currentHour > 12) {
+    twelvepm.addClass('past');
+  } else {
+    twelvepm.addClass('future');
+  }
+
+  if (currentHour == 13){
+    onepm.addClass('present');
+  } else if (currentHour > 13) {
+    onepm.addClass('past');
+  } else {
+    onepm.addClass('future');
+  }
+
+  if (currentHour == 14){
+    twopm.addClass('present');
+  } else if (currentHour > 14) {
+    twopm.addClass('past');
+  } else {
+    twopm.addClass('future');
+  }
+
+  if (currentHour == 15){
+    threepm.addClass('present');
+  } else if (currentHour > 15) {
+    threepm.addClass('past');
+  } else {
+    threepm.addClass('future');
+  }
+
+  if (currentHour == 16){
+    fourpm.addClass('present');
+  } else if (currentHour > 16) {
+    fourpm.addClass('past');
+  } else {
+    fourpm.addClass('future');
+  }
+
+  if (currentHour == 17){
+    fivepm.addClass('present');
+  } else if (currentHour > 17) {
+    fivepm.addClass('past');
+  } else {
+    fivepm.addClass('future');
+  }
+
+  //event listener to save user input to local storage using container ID
+  $(".saveBtn").on('click', function() {
+    var keyID = $(this).parent().attr("id");
+    var userInput = $(this).parent().children("textarea").val();
+    localStorage.setItem(keyID, userInput);
+  })
+
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 });
-
-main();
